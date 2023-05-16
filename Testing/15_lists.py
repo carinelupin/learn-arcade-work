3284992c27b4ab66fe74493c4333f3a52c75ff6f
carinelupin <carinelupin58@gmail.com>
2023-05-16 15:07:55 +0200

    lists

---
+++ Testing/15_lists.py
@@ -0,0 +1,29 @@
+"""
+list: to store a lot of data
+We can use a list to store multiple characters on screen of a game, list of items in shopping cart
+
+DATA TYPES:
+- string (short for "string of characters")
+- integer
+- floating point
+- boolean
+- lists
+- tuples
+- arrays
+
+to display what type of data a value is:
+print(type(3))
+ -----> <class 'int'>
+
+lists are similar to arrays
+a list can be resized, but array can't
+
+to create a list:
+x = [10, 20]
+print(x)
+ -----> [10, 20]
+
+print individual items in list:
+print(x[0])
+ -----> 10
+"""
