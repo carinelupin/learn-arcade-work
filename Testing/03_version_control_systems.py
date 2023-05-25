59496059faaebeab8bb02f5d34e123e31e453231
carinelupin <carinelupin58@gmail.com>
2023-05-25 12:16:59 +0200

    taking notes

---
+++ Testing/03_version_control_systems.py
@@ -0,0 +1,8 @@
+"""
+COMMIT & UPLOAD --- whenever ur done w/ session of programming
+1. Open command prompt (git bash)
+2. Type cd Desktop, followed by cd learn-arcade-work
+3. Type git add -A
+4. Type git commit -m "blah blah blah"  --- update comment to whatever you did
+5. Type git push
+"""
--- Testing/16_Classes-Constractors-Attributes.py
+++ Testing/16_Classes-Constractors-Attributes.py
@@ -17,21 +17,38 @@ object-oriented programming:
 we need code that'll create our attributes (vars) & set them to default vals
 i.e. __init__ method
 
-- any function in a class -- called a method
+- any function in a class -- called a "method"
 - the initialization (first) method is called automatically
 - methods invoked automatically also called a "magic method" & also "dunder methods"
 - "magic method" names are surrounded w double underscores - (2 back, 2 front)
-- 2 underscores -- also called a dunder
+- 2 underscores -- also called a "dunder"
 - all methods in a class have at least 1 parameter & 1st parameter is always self
 
 //////// DEFINING CLASS ATTRIBUTES
-- if not put "self" in front of vars, computer would forget vars once init func was done
+- like functions, variables created in methods are forgotten after method is done running,
+- if you want to keep it, return as value
 
+- the "self" parameter refers to memory associated w each instance of the class
+- use "self" to create vars that keep value for as long as object exists
+- attributes, fields, instance variable --- variables that exist as part of a class
+- attributes must be set to a default value
+- that value is often 0, an empty string, or special value "None"
 
-class E.g.
+- if not put "self" in front of vars, computer would forget vars once __init__ func was done
 
+- the __init__ is a special method that may be referred to as a "constructor"
+- in other langs, term "constructor" is generic term used to refer to that lang equivalent to __init__ method
 
+- the "self" is like pronoun "my"
+- inside class we talk about my var --- e.g. my name, my city, my car, etc.
+- don't use self outside class
+- bc like "my" it means someone different if said by different person
+
+##### Objects
+////////
 """
+
+# class E.g.
 
 
 class Character:
